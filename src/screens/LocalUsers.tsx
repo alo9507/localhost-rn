607dde681e6440ac3dccfcf64cc4a0f3b26c0bb2
Andrew <alo42@georgetown.edu>
2020-11-28 17:42:53 -0500

    adds visibility toggle

--- src/screens/LocalUsers.tsx
+++ src/screens/LocalUsers.tsx
@@ -4,6 +4,7 @@ import StoreContext from "../store/StoreContext";
 import EZAuthManager from "../service/authentication/AuthManager/EZAuthManager";
 import User from "../models/User"
 import AppState from "../models/AppState"
+import { Switch } from "react-native"
 
 const initialState = { id: "mynewid", name: "", location: "" };
 
@@ -11,8 +12,14 @@ const LocalUsers = (props) => {
   const [formState, setFormState] = useState(initialState);
   const [location, setLocation] = useState({ latitude: 24.22244098031902, longitude: 23.125367053780863 });
   const [appState, setAppState] = React.useContext(StoreContext);
+  const [isVisible, setIsVisible] = useState(appState.user.isVisible)
 
-  const [visibilityControls, setVisibilityControls] = useState({
+  const toggleSwitch = async () => {
+    const users = await appState.userRepository.updateUser({ id: appState.user.id, isVisible: !isVisible })
+    setIsVisible(previousState => !previousState);
+  }
+
+  const [showMeCriteria, setShowMeCriteria] = useState({
     id: appState.user.id,
     sex: appState.user.showMeCriteria.sex,
     age: appState.user.showMeCriteria.age
@@ -33,22 +40,20 @@ const LocalUsers = (props) => {
   const [state, setState] = useState(initial)
 
   useEffect(() => {
-    console.log(JSON.stringify(appState.user))
     async function getUsers() {
       try {
-        console.log(appState.user.id)
         const users = await appState.userRepository.updateLocationGetUsers({ id: appState.user.id, ...location })
         setState({ ...state, users, loading: false })
       } catch (e) {
-        setState({ ...state, loading: false, error: true })
+        setState({ ...state, loading: false, error: e })
       }
     }
 
     getUsers()
-  }, [])
+  }, [isVisible])
 
   useEffect(() => {
-  }, [visibilityControls])
+  }, [isVisible])
 
   useEffect(() => {
     props.navigation.setOptions({ title: appState.user?.name ? appState.user.name : "No Name" });
@@ -69,9 +74,19 @@ const LocalUsers = (props) => {
 
   return (
     <View style={styles.container}>
+      <View style={styles.container}>
+        <Text>isVisible: {isVisible}</Text>
+        <Switch
+          trackColor={{ false: "#767577", true: "#81b0ff" }}
+          thumbColor={isVisible ? "#f5dd4b" : "#f4f3f4"}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isVisible}
+        />
+      </View>
       <Button title="Sign Out" onPress={signOut} />
       <View>
-        <Text>VISIBILITY CONTROLS: AGE: {visibilityControls.age[0]} / {visibilityControls.age[1]} SEX: {visibilityControls.sex}</Text>
+        <Text>VISIBILITY CONTROLS: AGE: {showMeCriteria.age[0]} / {showMeCriteria.age[1]} SEX: {showMeCriteria.sex}</Text>
       </View>
       {state.users.map((user, index) => (
         <View key={user.id ? user.id : index} style={styles.user}>
