491a03c8e1861ae9571a3405baa0b3b880b999d1
Ankit <imobiledevzsolutions@gmail.com>
2020-12-18 10:41:54 +0530

    added intro to first screen

--- src/screens/FirstLaunch/FirstLaunch.tsx
+++ src/screens/FirstLaunch/FirstLaunch.tsx
@@ -1,11 +1,81 @@
-import React, { useEffect, useState } from "react";
-import { Text } from "react-native";
+import React from "react";
+import { Text, View, SafeAreaView, Image, StyleSheet } from "react-native";
+import AppIntroSlider from 'react-native-app-intro-slider';
+
+const slides = [
+    {
+      key: 'one',
+      title: 'Title 1',
+      text: 'Description.\nSay something cool',
+      image: {uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png'},
+      backgroundColor: '#59b2ab',
+    },
+    {
+      key: 'two',
+      title: 'Title 2',
+      text: 'Other cool stuff',
+      image: {uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png'},
+      backgroundColor: '#febe29',
+    },
+    {
+      key: 'three',
+      title: 'Rocket guy',
+      text: 'I\'m already out of descriptions\n\nLorem ipsum bla bla bla',
+      image: {uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png'},
+      backgroundColor: '#22bcb5',
+    }
+  ];
+
+  const styles = StyleSheet.create({
+    slide: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'pink',
+    },
+    image: {
+      width: 320,
+      height: 320,
+      marginVertical: 32,
+    },
+    text: {
+      // color: 'rgba(255, 255, 255, 0.8)',
+      textAlign: 'center',
+    },
+    title: {
+      fontSize: 22,
+      // color: 'white',
+      textAlign: 'center',
+    },
+    container: {flex: 1},
+  });
 
 const FirstLaunch = (props) => {
+    const renderItem = ({ item }) => {
+      const bgStyle = { backgroundColor: item.backgroundColor }
+        return (
+            <View style={[styles.slide, bgStyle]}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Image source={item.image} style={styles.image} />
+              <Text style={styles.text}>{item.text}</Text>
+            </View>
+          );
+    };
+
+    const onDone = () => {};
+
+    const keyExtractor = (item) => item.title;
 
     return (
         <>
-            <Text>FIRST LAUNCH SCREEN</Text>
+          <SafeAreaView style={styles.container}>
+            <AppIntroSlider
+                renderItem={renderItem}
+                data={slides}
+                onDone={onDone}
+                keyExtractor={keyExtractor}
+            />
+          </SafeAreaView>
         </>
     );
 };
