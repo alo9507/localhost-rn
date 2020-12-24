450e2e7b2f2b72c6788279c1b746de3073b6c523
Andrew <alo42@georgetown.edu>
2020-12-24 12:18:32 -0500

    adds venn svg

---
+++ src/components/Venn.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+const Venn = () => {
+    return (
+        <svg width="212px" height="205px" viewBox="0 0 212 205" version="1.1">
+            <title>Venn</title>
+            <g id="Splash" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" fill-opacity="0.508741259">
+                <g transform="translate(-101.000000, -328.000000)" fill="#00D6CC" id="Group">
+                    <g transform="translate(77.000000, 304.500000)" id="Venn">
+                        <ellipse id="Oval-Copy" transform="translate(170.229867, 90.000000) rotate(45.000000) translate(-170.229867, -90.000000) " cx="170.229867" cy="90" rx="45" ry="81"></ellipse>
+                        <ellipse id="Oval-Copy-3" transform="translate(90.000000, 162.862937) rotate(-135.000000) translate(-90.000000, -162.862937) " cx="90" cy="162.862937" rx="45" ry="81"></ellipse>
+                        <ellipse id="Oval-Copy-2" transform="translate(92.176939, 92.400880) rotate(-45.000000) translate(-92.176939, -92.400880) " cx="92.1769391" cy="92.4008799" rx="45" ry="81"></ellipse>
+                        <ellipse id="Oval-Copy-4" transform="translate(168.052928, 160.462057) rotate(-225.000000) translate(-168.052928, -160.462057) " cx="168.052928" cy="160.462057" rx="45" ry="81"></ellipse>
+                    </g>
+                </g>
+            </g>
+        </svg>
+    )
+}
+
+export default Venn
--- src/screens/Splash/Splash.tsx
+++ src/screens/Splash/Splash.tsx
@@ -1,13 +1,24 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import Venn from "../../components/Venn"
 
 const Splash = (props) => {
 
     return (
-        <>
-            <Text>Splash</Text>
-        </>
+        <View style={styles.container}>
+            <Text>localhost</Text>
+            <Venn />
+        </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignSelf: 'center',
+        backgroundColor: "#4450C7"
+    },
+});
 
 export default Splash;
