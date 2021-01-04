a6a9d98775b6341b3285f38e604cad140af12d90
Andrew <alo42@georgetown.edu>
2021-01-03 21:46:55 -0500

    adds location capabilities

--- src/screens/Onboarding/OnboardingLocation.tsx
+++ src/screens/Onboarding/OnboardingLocation.tsx
@@ -1,25 +1,75 @@
-import React, { useContext, useState } from "react"
+import React, { useReducer, useContext } from "react"
 import { View, Text, Button } from "react-native"
 import styles from "./OnboardingStyle"
 import styled from "styled-components/native";
 import StoreContext from "../../store/StoreContext"
-import OnboardingForm from "./OnboardingForm"
+import * as Location from 'expo-location';
 
 const OnboardingLocation = ({ item, goToNext, slideNumber }) => {
-    const [appState, setAppState] = useContext(StoreContext);
+  const [appState, setAppState] = useContext(StoreContext);
 
-    async function submitAndGoToNext() {
+  const [state, dispatch] = useReducer(
+    (prevState, action) => {
+      switch (action.type) {
+        case 'LOADING':
+          return {
+            loading: true,
+            error: null,
+            location: null
+          };
+        case 'ERROR':
+          return {
+            loading: false,
+            error: action.payload,
+            location: null
+          };
+        case 'LOCATION':
+          return {
+            loading: false,
+            error: null,
+            location: action.payload
+          };
+        default:
+          throw new Error(`UNKNOWN ACTION: ${action.type}`)
+      }
+    },
+    {
+      loading: false,
+      error: null,
+      location: null
+    }
+  );
 
+  async function getLocation() {
+    let { status } = await Location.requestPermissionsAsync();
+    if (status !== 'granted') {
+      dispatch({ type: "ERROR", payload: 'Permission to access location was denied' })
+      return;
     }
 
-    const bgStyle = { backgroundColor: item.backgroundColor }
-    return (
-        <View style={[styles.slide, bgStyle]}>
-            <Container>
-                <Text>Location</Text>
-            </Container>
-        </View >
-    )
+    dispatch({ type: "LOADING" })
+    let location = await Location.getCurrentPositionAsync({});
+    dispatch({ type: "LOCATION", payload: location })
+  }
+
+  let text = 'Waiting..';
+  if (state.error) {
+    text = state.error;
+  } else if (state.location) {
+    text = JSON.stringify(state.location);
+  }
+
+  const bgStyle = { backgroundColor: item.backgroundColor }
+  return (
+    <View style={[styles.slide, bgStyle]}>
+      <Container>
+        <View style={styles.container}>
+          <Text>{text}</Text>
+          <Button onPress={getLocation} title="Enable Location" />
+        </View>
+      </Container>
+    </View >
+  )
 }
 
 const Input = styled.TextInput`
